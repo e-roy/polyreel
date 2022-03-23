459884c7f873e952a83dfa70ae5e78862e722479
e-roy <ericroupe@gmail.com>
2022-03-23 00:32:34 -0400

    change config

--- packages/next-app/next.config.js
+++ packages/next-app/next.config.js
@@ -1,13 +1,5 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  images: {
-    domains: [
-      "",
-      "ipfs.io",
-      "ipfs.infura.io",
-      "lh3.googleusercontent.com",
-      "statics-mumbai-lens.s3.amazonaws.com",
-    ],
-  },
+  trailingSlash: true,
 };
