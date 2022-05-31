05a005cfb135cc940ec0455723f08912e2b2e097
e-roy <ericroupe@gmail.com>
2022-05-31 17:35:03 -0400

    fix data

--- packages/next-app/components/cards/FollowersCard.tsx
+++ packages/next-app/components/cards/FollowersCard.tsx
@@ -9,11 +9,11 @@ export const FollowersCard = ({ profile }: any) => {
     return (
       <div
         className="mb-2 p-2 border border-stone-400 shadow rounded w-full text-sm cursor-pointer hover:shadow-xl"
-        onClick={() => router.push(`/profile/${profile.handle}`)}
+        // onClick={() => router.push(`/profile/${profile.handle}`)}
       >
         <div className="flex justify-between mb-2 pr-4">
           <div className="flex">
-            <Avatar profile={profile} size={"medium"} />
+            <Avatar size={"medium"} />
 
             <div className="ml-4 px-2 py-1 my-auto text-stone-800  rounded-xl">
               <div className="my-auto font-semibold text-sm sm:text-lg">
