c1c5b24031da6ea526343d3e7c67cda6a1ffbbaa
e-roy <ericroupe@gmail.com>
2022-05-31 18:05:52 -0400

    external link

--- packages/next-app/components/cards/FollowersCard.tsx
+++ packages/next-app/components/cards/FollowersCard.tsx
@@ -1,6 +1,7 @@
 import { useRouter } from "next/router";
 import { Avatar } from "@/components/elements";
-import { addressShorten } from "@/utils/address-shorten";
+import { addressHalf } from "@/utils/address-shorten";
+import { ExternalLinkIcon } from "@heroicons/react/outline";
 
 export const FollowersCard = ({ profile }: any) => {
   const router = useRouter();
@@ -8,21 +9,23 @@ export const FollowersCard = ({ profile }: any) => {
   if (!profile) return null;
   if (!profile.defaultProfile)
     return (
-      <a href={`https://polygonscan.com/address/${profile.address}`}>
-        <div
-          className="mb-2 p-2 border border-stone-400 shadow rounded w-full text-sm cursor-pointer hover:shadow-xl"
-          // onClick={() => router.push(`/profile/${profile.handle}`)}
-        >
+      <a
+        href={`https://polygonscan.com/address/${profile.address}`}
+        target="_blank"
+        rel="noreferrer noopener"
+      >
+        <div className="mb-2 p-2 border border-stone-400 shadow rounded w-full text-sm cursor-pointer hover:shadow-xl">
           <div className="flex justify-between mb-2 pr-4">
             <div className="flex">
-              <Avatar size={"medium"} />
+              <Avatar profile={profile} size={"medium"} />
 
               <div className="ml-4 px-2 py-1 my-auto text-stone-800  rounded-xl">
                 <div className="my-auto font-semibold text-sm sm:text-lg">
-                  {addressShorten(profile.address)}
+                  {addressHalf(profile.address)}
+                  <ExternalLinkIcon className="h-5 w-5 ml-4 mt-0.5 hover:text-sky-600" />
                 </div>
                 <div className="my-auto font-semibold text-xs">
-                  {addressShorten(profile.address)}
+                  {addressHalf(profile.address)}
                 </div>
               </div>
             </div>
--- packages/next-app/utils/address-shorten.ts
+++ packages/next-app/utils/address-shorten.ts
@@ -11,3 +11,17 @@ export const addressShorten = (address: string) => {
     )}`.toLowerCase();
   }
 };
+
+export const addressHalf = (address: string) => {
+  if (
+    address.includes(".eth") ||
+    address === "" ||
+    address === "Not connected"
+  ) {
+    return address;
+  } else {
+    return `${address.substring(0, 6)}...${address.substring(
+      address.length - 8
+    )}`.toLowerCase();
+  }
+};
